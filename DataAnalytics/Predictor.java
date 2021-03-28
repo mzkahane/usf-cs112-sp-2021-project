2bfa593fb1cddab00bff7e596b09efd69a1a0e5b
mzkahane <80354992+mzkahane@users.noreply.github.com>
2021-03-28 18:48:31 -0400

    Added Predictor.java

---
+++ DataAnalytics/Predictor.java
@@ -0,0 +1,13 @@
+package project1;
+
+import java.util.ArrayList;
+
+public abstract class Predictor {
+	abstract ArrayList<DataPoint> readData(String filename);
+	
+	abstract String test(DataPoint data);
+	
+	abstract Double getAccuracy(ArrayList<DataPoint> data);
+	
+	abstract Double getPrecision(ArrayList<DataPoint> data);
+}
